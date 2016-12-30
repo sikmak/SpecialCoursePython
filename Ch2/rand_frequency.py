eea27db7b3a8a5c08f41b3f5a869853f3b0d72fd
sikmak <sikmak@mail.ru>
2016-12-30 19:28:40 +0300

    Исправил2

--- Ch2/rand_frequency.py
+++ Ch2/rand_frequency.py
@@ -4,7 +4,7 @@ Created on Sun Oct 30 19:00:41 2016
 
 @author: Никита
 """
-
+import os
 from numpy import random
 
 N=int(input("Введите N:"))
@@ -15,8 +15,9 @@ for i in range(N):
     vrem=random.randint(1,7)
     x.append(vrem)
     if vrem==6:
-        M+=1
+        M+=1.
 print("Вероятность:")
 print(str(M/N))
 print("Все варианты:")
 print(x)
+os.system("pause")
