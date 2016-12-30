2ca27435645ee6627619b29b4465cc6dc6e93491
sikmak <sikmak@mail.ru>
2016-12-30 18:25:33 +0300

    Исправил

--- Ch2/area_and_circumference_func.py
+++ Ch2/area_and_circumference_func.py
@@ -4,14 +4,18 @@ Created on Sun Oct 23 16:32:55 2016
 
 @author: Никита
 """
+import os
 from math import pi
 
-def cuircleLength_square(r):
-    C = 2.*pi*r
-    A = 2*pi*r**2
-    return C,A
+def cuircle_square(r):
+    S = pi*r**2
+    return S
     
+def cuircle_length(r):
+     L = 2*pi*r
+     return L
     
 r=float(input("Введите радиус окружности: "))
-print("Длина окружности, Площадь окружности:")
-print(cuircleLength_square(r))
+print("Длина окружности: "+str(cuircle_length(r))+"\n")
+print("Площадь круга: "+str(cuircle_square(r)))
+os.system("pause")
