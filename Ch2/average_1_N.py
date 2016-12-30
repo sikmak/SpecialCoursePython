0913b055e5ef961812fff78894e7147321cc1793
sikmak <sikmak@mail.ru>
2016-12-30 18:50:38 +0300

    Исправил

--- Ch2/average_1_N.py
+++ Ch2/average_1_N.py
@@ -4,14 +4,18 @@ Created on Sun Oct 23 19:20:20 2016
 
 @author: Никита
 """
-
+import os
 def everage(N):
-    n=N/2;
-    print(n)
-    if isinstance( n, int ):
-         return n-.5
-    else:
-        return n+.5
+    print("i=1")
+    print("Среднее значение равно:1")
+    for i in range(2,N+1):
+        n=i/2;
+        print("i="+ str(i))
+        if isinstance( n, int ):
+            print("Среднее значение равно:"+str(n-.5))
+        else:
+            print("Среднее значение равно:"+str(n+.5))
 
 N = int(input("Введите N: "))
-print("Среднее значение = "+str(everage(N)))
+everage(N)
+os.system("pause")
